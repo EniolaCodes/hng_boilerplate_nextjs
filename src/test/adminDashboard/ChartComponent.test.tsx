b04633f27a2fe2175fc99ae19f76143c744184c6
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-21 17:09:15 +0100

    test: add ChartComponent tests for data rendering and tooltip functionality

--- src/test/adminDashboard/ChartComponent.test.tsx
+++ src/test/adminDashboard/ChartComponent.test.tsx
@@ -6,30 +6,25 @@ import { chartConfig, chartData } from "~/components/adminDashboard/chartData";
 describe("chart Component", () => {
   it("renders the chart with the correct data", () => {
     expect.assertions(1);
-    // Calculate total assertions: Adjust based on the number of actual assertions
-    const totalAssertions = chartData.length + 5 + 1; // Length of chartData + Y axis labels + bars count
+    const totalAssertions = chartData.length + 5 + 1;
     expect.assertions(totalAssertions);
 
     render(<Chart chartData={chartData} chartConfig={chartConfig} />);
 
-    // Check X axis labels
     for (const data of chartData) {
       expect(screen.getByText(data.month.slice(0, 3))).toBeInTheDocument();
     }
 
-    // Check Y axis labels
     for (const value of [0, 1500, 3000, 4500, 6000]) {
       expect(screen.getByText(`$${value}`)).toBeInTheDocument();
     }
 
-    // Check if the number of bars matches the data length
     const bars = screen.getAllByRole("img", { hidden: true });
     expect(bars).toHaveLength(chartData.length);
   });
 
   it("renders the tooltip on bar hover", () => {
-    // Ensure expect.assertions is the first statement
-    expect.assertions(1); // Expect one assertion for checking tooltip presence
+    expect.assertions(1);
 
     render(<Chart chartData={chartData} chartConfig={chartConfig} />);
 
